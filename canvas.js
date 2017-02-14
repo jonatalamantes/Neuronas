81d7faab89d1137d94729a6b68d188956229dc9d
jonatalamantes <jonathan_s_pisis@hotmail.com>
2017-02-14 10:52:31 -0600

    Agregamiento de tabla de pesos

--- canvas.js
+++ canvas.js
@@ -1,3 +1,229 @@
+var valE = 0;
+var valS = 1;
+
+var entradaCero   = [valE, valS, valS, valS, valE, 
+                     valS, valE, valE, valE, valS, 
+                     valS, valE, valE, valS, valS, 
+                     valS, valE, valS, valE, valS, 
+                     valS, valS, valE, valE, valS, 
+                     valS, valE, valE, valE, valS, 
+                     valE, valS, valS, valS, valE];
+
+var entradaUno    = [valE, valE, valS, valE, valE, 
+                     valE, valS, valS, valE, valE, 
+                     valE, valE, valS, valE, valE, 
+                     valE, valE, valS, valE, valE, 
+                     valE, valE, valS, valE, valE, 
+                     valE, valE, valS, valE, valE, 
+                     valE, valS, valS, valS, valE];
+
+var entradaDos    = [valE, valS, valS, valS, valE, 
+                     valS, valE, valE, valE, valS, 
+                     valE, valE, valE, valE, valS, 
+                     valE, valE, valE, valS, valE,
+                     valE, valE, valS, valE, valE, 
+                     valE, valS, valE, valE, valE, 
+                     valS, valS, valS, valS, valS];
+
+var entradaTres   = [valS, valS, valS, valS, valS, 
+                     valE, valE, valE, valS, valE, 
+                     valE, valE, valS, valE, valE, 
+                     valE, valE, valE, valS, valE, 
+                     valE, valE, valE, valE, valS, 
+                     valS, valE, valE, valE, valS, 
+                     valE, valS, valS, valS, valE];
+
+var entradaCuatro = [valE, valE, valE, valS, valE, 
+                     valE, valE, valS, valS, valE, 
+                     valE, valS, valE, valS, valE, 
+                     valS, valE, valE, valS, valE, 
+                     valS, valS, valS, valS, valS, 
+                     valE, valE, valE, valS, valE, 
+                     valE, valE, valE, valS, valE];
+
+var entradaCinco = [valS, valS, valS, valS, valS, 
+                    valS, valE, valE, valE, valE, 
+                    valS, valS, valS, valS, valE, 
+                    valE, valE, valE, valE, valS, 
+                    valE, valE, valE, valE, valS, 
+                    valS, valE, valE, valE, valS, 
+                    valE, valS, valS, valS, valE];
+
+var entradaSeis = [valE, valE, valS, valS, valE, 
+                   valE, valS, valE, valE, valE, 
+                   valS, valE, valE, valE, valE, 
+                   valS, valS, valS, valS, valE, 
+                   valS, valE, valE, valE, valS, 
+                   valS, valE, valE, valE, valS, 
+                   valE, valS, valS, valS, valE];
+
+var entradaSiete = [valS, valS, valS, valS, valS, 
+                    valE, valE, valE, valE, valS, 
+                    valE, valE, valE, valS, valE, 
+                    valE, valE, valS, valE, valE, 
+                    valE, valS, valE, valE, valE, 
+                    valE, valS, valE, valE, valE,
+                    valE, valS, valE, valE, valE];
+
+var entradaOcho = [valE, valS, valS, valS, valE, 
+                   valS, valE, valE, valE, valS, 
+                   valS, valE, valE, valE, valS, 
+                   valE, valS, valS, valS, valE, 
+                   valS, valE, valE, valE, valE, 
+                   valS, valE, valE, valE, valS, 
+                   valE, valS, valS, valS, valE];
+
+var entradaNueve = [valE, valS, valS, valS, valE, 
+                    valS, valE, valE, valE, valS, 
+                    valS, valE, valE, valE, valS, 
+                    valE, valS, valS, valS, valS, 
+                    valE, valE, valE, valE, valS, 
+                    valE, valE, valE, valS, valE, 
+                    valE, valS, valS, valE, valE];
+
+var entradaGuion = [valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valS, valS, valS, valS, valS, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE];
+
+var entradaPunto = [valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valE, valE, valE, valE, 
+                    valE, valS, valS, valE, valE,
+                    valE, valS, valS, valE, valE];
+
+var salidaCero =   [valS,valE,valE,valE,valE,valE,valE,valE,valE,valE,valE,valE];
+var salidaUno =    [valE,valS,valE,valE,valE,valE,valE,valE,valE,valE,valE,valE];
+var salidaDos =    [valE,valE,valS,valE,valE,valE,valE,valE,valE,valE,valE,valE];
+var salidaTres =   [valE,valE,valE,valS,valE,valE,valE,valE,valE,valE,valE,valE];
+var salidaCuatro = [valE,valE,valE,valE,valS,valE,valE,valE,valE,valE,valE,valE];
+var salidaCinco =  [valE,valE,valE,valE,valE,valS,valE,valE,valE,valE,valE,valE];
+var salidaSeis =   [valE,valE,valE,valE,valE,valE,valS,valE,valE,valE,valE,valE];
+var salidaSiete =  [valE,valE,valE,valE,valE,valE,valE,valS,valE,valE,valE,valE];
+var salidaOcho =   [valE,valE,valE,valE,valE,valE,valE,valE,valS,valE,valE,valE];
+var salidaNueve =  [valE,valE,valE,valE,valE,valE,valE,valE,valE,valS,valE,valE];
+var salidaGuion =  [valE,valE,valE,valE,valE,valE,valE,valE,valE,valE,valS,valE];
+var salidaPunto =  [valE,valE,valE,valE,valE,valE,valE,valE,valE,valE,valE,valS];
+
+var entradasRed = [entradaCero,   entradaUno,   entradaDos,   entradaTres,
+                   entradaCuatro, entradaCinco, entradaSeis,  entradaSiete,
+                   entradaOcho,   entradaNueve, entradaPunto, entradaGuion];
+
+var salidasRed = [salidaCero,   salidaUno,   salidaDos,   salidaTres,
+                  salidaCuatro, salidaCinco, salidaSeis,  salidaSiete,
+                  salidaOcho,   salidaNueve, salidaPunto, salidaGuion];
+/* Red Neuronal de Antes 
+var redNeuronal = new RedNeuronal(0.2, false);
+redNeuronal.construir([35,30,25,20,15,12], 0.8);
+
+//entrenamiento de la red (Tiempo estimado de procesamiento: 06:08)
+var fechaInicio = new Date();
+
+
+for (var i = 0; i < 1500; i++)
+{
+    for (var j = 0; j < entradasRed.length; j++)
+    {
+        redNeuronal.entrenar(entradasRed[j], salidasRed[j]);
+    }
+
+    console.log("iteracion: " + i + " " + redNeuronal.getError());
+}
+
+var fechaFin = new Date();
+var dif= fechaFin - fechaInicio; 
+var difSeg = Math.floor(dif/1000);
+var segundos = difSeg % 60; 
+var difMin = Math.floor(difSeg/60); 
+var minutos = difMin % 60; 
+var difHs = Math.floor(difMin/60); 
+var horas = difHs % 24;
+
+console.log(horas+":"+minutos+":"+segundos);
+
+console.log(redNeuronal.getError());
+
+//redNeuronal.logString();
+*/
+
+/* Creacion de la red de ENCOG */
+var network = ENCOG.BasicNetwork.create( [
+  ENCOG.BasicLayer.create(ENCOG.ActivationSigmoid.create(), 35, 1),
+  ENCOG.BasicLayer.create(ENCOG.ActivationSigmoid.create(), 20, 1),
+  ENCOG.BasicLayer.create(ENCOG.ActivationSigmoid.create(), 15, 1),
+  ENCOG.BasicLayer.create(ENCOG.ActivationSigmoid.create(), 12, 1)] );
+network.randomize();
+
+var train = ENCOG.PropagationTrainer.create(network,entradasRed,salidasRed,"RPROP",0,0);
+
+var iteration = 1;
+do
+{
+	train.iteration();
+	var str = "Training Iteration #" + iteration + ", Error: " + train.error;
+	console.log(str);
+	iteration++;
+} 
+while(iteration < 1000 && train.error > 0.0001);
+
+function clasificar()
+{
+    clases = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Punto", "Guion"];
+
+    var arregloEntradas=[];
+    for(i=1,j=35;i<=j;i++)
+    {
+    	var valor = valS;
+
+    	if (document.getElementById(i).getAttribute("class") == "noSeleccionado")
+    	{
+    		var valor = valE;
+    	}
+    	
+        arregloEntradas.push(valor);
+    }
+
+	var output = [];
+	network.compute(arregloEntradas,output);
+
+    var mayor    = 0;
+    var posMayor = 0;
+
+    for (var i = 0; i < output.length; i++)
+    {
+        var valor = output[i];
+
+        if (valor > mayor)
+        {   
+            posMayor = i;
+            mayor    = valor;
+        }
+
+        console.log($("#c" + (i+1)));
+        $("#c" + (i+1)).children()[0].innerHTML = clases[i];
+        $("#c" + (i+1)).children()[1].innerHTML = valor;
+        $("#c" + (i+1)).children()[2].innerHTML = Math.round(valor);
+    }
+
+    $("#resultado").val(clases[posMayor]);
+}
+
+function limpiar()
+{
+    for(i=1,j=35;i<=j;i++)
+    {
+        var cuadro = document.getElementById(i);
+        cuadro.setAttribute("class","noSeleccionado");
+    }
+}
+
+limpiar();
+
 var id;
 
 for (var i=1; i<36; i++) {
@@ -8,6 +234,3 @@ for (var i=1; i<36; i++) {
         $(this).toggleClass('seleccionado');
     });
 }
-
-
-
